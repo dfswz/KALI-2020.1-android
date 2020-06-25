434f29d6d1678aee9195bec8d825b70a3c4d3081
dfswz <63559375+dfswz@users.noreply.github.com>
2020-06-25 13:17:25 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,11 @@
 pkg upgrade -y
 pkg update -y
 pip install lolcat
-pkg install fish -y
 pkg upgrade -y
 pkg update -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install fish -y
+pkg install wget -y
+pkg upgrade -y | lolcat
+pkg update -y | lolcat
